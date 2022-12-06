a4652b3fc2ef53ed62da02b72c9694f325572634
pankaj <pjadhav138@gmail.com>
2022-12-07 02:04:03 +0530

    UI changes

--- app/src/main/java/com/example/gexemi/DashboardActivity.java
+++ app/src/main/java/com/example/gexemi/DashboardActivity.java
@@ -8,13 +8,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    TextView tv_qr, tv_policy,tv_users, tv_myprofile,dashboard_username,dashboard_vendorID;
+    TextView tv_qr, tv_policy,tv_users, tv_myprofile,dashboard_username,dashboard_vendorID,support;
     Dialog mDialog;
 
     SharedPreferences preferences;
@@ -23,13 +24,22 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
+//Wa.me/919892580308
+        getSupportActionBar().setTitle("Go Electronix pvt. ltd.");
         tv_qr = findViewById(R.id.TV_qr);
         tv_policy = findViewById(R.id.TV_policy);
         tv_users = findViewById(R.id.TV_user);
         tv_myprofile = findViewById(R.id.TV_myprofile);
         dashboard_username = findViewById(R.id.dashboard_username);
         dashboard_vendorID = findViewById(R.id.dashboard_vendorID);
+        support = findViewById(R.id.support);
+        support.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send/?phone=919892580308&text=Hi"));
+                startActivity(intent);
+            }
+        });
         mDialog = new Dialog(this);
 
         preferences = getSharedPreferences("VendorDetails",MODE_PRIVATE);
